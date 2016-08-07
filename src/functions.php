1a7baeac4ef65ba85cfde2142b447b92f9e3de5e
tetreum <basura@yelidmod.com>
2016-08-07 15:36:06 +0200

    Add files via upload

---
+++ src/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+* Perfectly prints any kind of var without caring about enviroment (console or normal deb)
+*/
+if (!function_exists("p")) {
+	function p()
+	{
+	    $consolePrint = false;
+
+	    if (!isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == null) {
+	        $consolePrint = true;
+	    }
+
+	    if (!$consolePrint) {
+	        echo '<pre>';
+	    }
+	    
+	    $args = func_get_args();
+
+	    foreach($args as $var)
+	    {
+	        if ($var == null || $var == '') {
+	            var_dump($var);
+	        }
+	        elseif(is_array($var) || is_object($var)) {
+	            print_r($var);
+	        } else {
+	            echo $var;
+	        }
+	        
+	        if (!$consolePrint) {
+	            echo '<br>';
+	        } else {
+	            echo "\n";
+	        }
+	    }
+	    if (!$consolePrint) {
+	        echo '</pre>';
+	    }
+	}
+}
