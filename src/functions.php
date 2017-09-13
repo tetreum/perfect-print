f2097f3404353d4464845017d04fbd3db526784f
tetreum <basura@yelidmod.com>
2017-09-13 08:56:44 +0200

    + pd method (print & die)

--- src/functions.php
+++ src/functions.php
@@ -40,3 +40,42 @@ if (!function_exists("p")) {
 	    }
 	}
 }
+
+if (!function_exists("pd")) {
+	function pd()
+	{
+	    $consolePrint = false;
+
+	    if (!isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == null) {
+	        $consolePrint = true;
+	    }
+
+	    if (!$consolePrint) {
+	        echo '<pre>';
+	    }
+	    
+	    $args = func_get_args();
+
+	    foreach($args as $var)
+	    {
+	        if ($var == null || $var == '') {
+	            var_dump($var);
+	        }
+	        elseif(is_array($var) || is_object($var)) {
+	            print_r($var);
+	        } else {
+	            echo $var;
+	        }
+	        
+	        if (!$consolePrint) {
+	            echo '<br>';
+	        } else {
+	            echo "\n";
+	        }
+	    }
+	    if (!$consolePrint) {
+	        echo '</pre>';
+	    }
+	    exit;
+	}
+}
